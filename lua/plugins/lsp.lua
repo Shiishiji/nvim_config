284be4acf433522ae0f9b1d5cef1401bdd054670
Damian Szopiński <damian.szopinski@verestro.com>
2025-10-11 03:45:12 +0200

    Remove omnisharp path - let it auto

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,9 +16,6 @@ return {
           },
         },
         omnisharp = {
-          cmd = {
-            vim.fn.stdpath("data") .. "/mason/bin/OmniSharp",
-          },
           enable_editorconfig_support = true,
           enable_roslyn_analyzers = true,
           organize_imports_on_format = true,
